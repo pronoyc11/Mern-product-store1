e409cdf9435abaa873574b5aabbd23c90d37d0db
pronoyc11 <pronoyc11@gmail.com>
2025-10-04 22:02:02 +0600

    fixed_error

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
-app.use(cors());
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+app.use(cors());
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
